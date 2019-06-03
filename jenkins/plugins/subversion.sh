5ff3d4fdcd265c19423e0e662c6dab24eed04f51
Quan Chen <chenq182@sina.com>
2019-06-03 09:10:01 +0800

    2019-06-03 09:10:01

---
+++ jenkins/plugins/subversion.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# https://plugins.jenkins.io/subversion
+#JAXB
+wget -c https://updates.jenkins.io/download/plugins/jaxb/2.3.0.1/jaxb.hpi
+#JDK Tool
+wget -c https://updates.jenkins.io/download/plugins/jdk-tool/1.2/jdk-tool.hpi
+#Script Security
+wget -c https://updates.jenkins.io/download/plugins/script-security/1.59/script-security.hpi
+#Command Agent Launcher
+wget -c https://updates.jenkins.io/download/plugins/command-launcher/1.3/command-launcher.hpi
+#Structs
+wget -c https://updates.jenkins.io/download/plugins/structs/1.19/structs.hpi
+#Pipeline: Step API
+wget -c https://updates.jenkins.io/download/plugins/workflow-step-api/2.19/workflow-step-api.hpi
+#Pipeline: SCM Step
+wget -c https://updates.jenkins.io/download/plugins/workflow-scm-step/2.7/workflow-scm-step.hpi
+#bouncycastle API
+wget -c https://updates.jenkins.io/download/plugins/bouncycastle-api/2.17/bouncycastle-api.hpi
+#Credentials
+wget -c https://updates.jenkins.io/download/plugins/credentials/2.1.19/credentials.hpi
+#SSH Credentials
+wget -c https://updates.jenkins.io/download/plugins/ssh-credentials/1.16/ssh-credentials.hpi
+#SCM API
+wget -c https://updates.jenkins.io/download/plugins/scm-api/2.4.1/scm-api.hpi
+#Pipeline: API
+wget -c https://updates.jenkins.io/download/plugins/workflow-api/2.34/workflow-api.hpi
+#JUnit
+wget -c https://updates.jenkins.io/download/plugins/junit/1.28/junit.hpi
+#MapDB API
+wget -c https://updates.jenkins.io/download/plugins/mapdb-api/1.0.9.0/mapdb-api.hpi
+#Subversion
+wget -c https://updates.jenkins.io/download/plugins/subversion/2.12.1/subversion.hpi
