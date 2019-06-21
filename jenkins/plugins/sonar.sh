c6ae572492061d6af30232104a66becfa2f5645e
Quan Chen <chenq182@sina.com>
2019-06-21 11:53:10 +0800

    2019-06-21 11:53:10

---
+++ jenkins/plugins/sonar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://plugins.jenkins.io/sonar
+#JDK Tool
+wget -c https://updates.jenkins.io/download/plugins/jdk-tool/1.2/jdk-tool.hpi
+#Script Security
+wget -c https://updates.jenkins.io/download/plugins/script-security/1.59/script-security.hpi
+#Command Agent Launcher
+wget -c https://updates.jenkins.io/download/plugins/command-launcher/1.3/command-launcher.hpi
+#Structs
+wget -c https://updates.jenkins.io/download/plugins/structs/1.19/structs.hpi
+#Credentials
+wget -c https://updates.jenkins.io/download/plugins/credentials/2.1.19/credentials.hpi
+#Plain Credentials
+wget -c https://updates.jenkins.io/download/plugins/plain-credentials/1.5/plain-credentials.hpi
+#SonarQube Scanner
+wget -c https://updates.jenkins.io/download/plugins/sonar/2.9/sonar.hpi
