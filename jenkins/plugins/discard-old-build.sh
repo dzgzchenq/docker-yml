f0095713184c51eb86e560b4c265b6dd4aefc2b1
Quan Chen <chenq182@sina.com>
2019-07-30 10:04:37 +0800

    2019-07-30 10:04:37

---
+++ jenkins/plugins/discard-old-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# https://plugins.jenkins.io/discard-old-build
+#JDK Tool
+wget -c https://updates.jenkins.io/download/plugins/jdk-tool/1.2/jdk-tool.hpi
+#Script Security
+wget -c https://updates.jenkins.io/download/plugins/script-security/1.59/script-security.hpi
+#Structs
+wget -c https://updates.jenkins.io/download/plugins/structs/1.19/structs.hpi
+#Pipeline: Step API
+wget -c https://updates.jenkins.io/download/plugins/workflow-step-api/2.19/workflow-step-api.hpi
+#SCM API
+wget -c https://updates.jenkins.io/download/plugins/scm-api/2.4.1/scm-api.hpi
+#Pipeline: API
+wget -c https://updates.jenkins.io/download/plugins/workflow-api/2.34/workflow-api.hpi
+#JUnit
+wget -c https://updates.jenkins.io/download/plugins/junit/1.28/junit.hpi
+#Command Agent Launcher
+wget -c https://updates.jenkins.io/download/plugins/command-launcher/1.3/command-launcher.hpi
+#bouncycastle API
+wget -c https://updates.jenkins.io/download/plugins/bouncycastle-api/2.17/bouncycastle-api.hpi
+#WMI Windows Agents
+wget -c https://updates.jenkins.io/download/plugins/windows-slaves/1.4/windows-slaves.hpi
+#Matrix Authorization Strategy
+wget -c https://updates.jenkins.io/download/plugins/matrix-auth/2.4.2/matrix-auth.hpi
+#OWASP Markup Formatter
+wget -c https://updates.jenkins.io/download/plugins/antisamy-markup-formatter/1.5/antisamy-markup-formatter.hpi
+#Matrix Project
+wget -c https://updates.jenkins.io/download/plugins/matrix-project/1.14/matrix-project.hpi
+#Discard Old Build
+wget -c https://updates.jenkins.io/download/plugins/discard-old-build/1.05/discard-old-build.hpi
